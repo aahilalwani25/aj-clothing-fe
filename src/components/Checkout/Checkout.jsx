d7dd561712b2edf1866694ad612b1c0e3b368125
Aahil Alwani <alwani.aahil25@gmail.com>
2025-02-09 18:26:26 +0500

    chore: 1. change the dummy products that the user has ordered into the actual products

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,14 +1,10 @@
 "use client";
 
-const dummyProducts = [
-  { id: 1, name: "Split Sneakers", size: 37, quantity: 2, price: 40, img: "/images/product10.webp" },
-  { id: 2, name: "Velvet Boots", size: 37, quantity: 2, price: 40, img: "/images/product11.webp" },
-  { id: 3, name: "Echo Elegance", size: 37, quantity: 2, price: 40, img: "/images/product14.webp" },
-  { id: 4, name: "Pumps", size: 37, quantity: 2, price: 40, img: "/images/product13.webp" },
-];
+import { useSelector } from "react-redux";
 
 export default function Checkout() {
-  const totalAmount = dummyProducts.reduce((acc, item) => acc + item.price, 0);
+  const cart = useSelector((state) => state.cart);
+  const totalAmount = cart?.items.reduce((acc, item) => acc + item.price, 0);
 
   return (
     <div className="font-sans bg-white">
@@ -17,17 +13,31 @@ export default function Checkout() {
           <div className="relative h-full">
             <div className="px-4 py-8 sm:overflow-auto sm:h-[calc(100vh-60px)]">
               <div className="space-y-4">
-                {dummyProducts.map((product) => (
-                  <div key={product.id} className="flex items-start gap-4">
+                {cart?.items?.map((product) => (
+                  <div
+                    key={product.product_id}
+                    className="flex items-start gap-4"
+                  >
                     <div className="w-32 h-28 max-lg:w-24 max-lg:h-24 flex p-3 shrink-0 bg-gray-200 rounded-md">
-                      <img src={product.img} className="w-full object-contain" alt={product.name} />
+                      <img
+                        src={`${process.env.NEXT_PUBLIC_API_REMOTE_URL}/v1/media/products/${product?.main_image}`}
+                        className="w-full object-contain"
+                        alt={product.title}
+                      />
                     </div>
                     <div className="w-full">
-                      <h3 className="text-sm lg:text-base text-gray-800">{product.name}</h3>
+                      <h3 className="text-sm lg:text-base text-gray-800">
+                        {product.title}
+                      </h3>
                       <ul className="text-xs text-gray-800 space-y-1 mt-3">
-                        <li className="flex flex-wrap gap-4">Size <span className="ml-auto">{product.size}</span></li>
-                        <li className="flex flex-wrap gap-4">Quantity <span className="ml-auto">{product.quantity}</span></li>
-                        <li className="flex flex-wrap gap-4">Total Price <span className="ml-auto">${product.price}</span></li>
+                        <li className="flex flex-wrap gap-4">
+                          Quantity{" "}
+                          <span className="ml-auto">{product.quantity}</span>
+                        </li>
+                        <li className="flex flex-wrap gap-4">
+                          Total Price{" "}
+                          <span className="ml-auto">${product.price}</span>
+                        </li>
                       </ul>
                     </div>
                   </div>
@@ -35,33 +45,77 @@ export default function Checkout() {
               </div>
             </div>
             <div className="md:absolute md:left-0 md:bottom-0 bg-gray-200 w-full p-4">
-              <h4 className="flex flex-wrap gap-4 text-sm lg:text-base text-gray-800">Total <span className="ml-auto">${totalAmount}</span></h4>
+              <h4 className="flex flex-wrap gap-4 text-sm lg:text-base text-gray-800">
+                Total <span className="ml-auto">${totalAmount}</span>
+              </h4>
             </div>
           </div>
         </div>
         <div className="max-w-4xl w-full h-max rounded-md px-4 py-8 sticky top-0">
-          <h2 className="text-2xl font-bold text-gray-800">Complete your order</h2>
+          <h2 className="text-2xl font-bold text-gray-800">
+            Complete your order
+          </h2>
           <form className="mt-8">
             <div>
-              <h3 className="text-sm lg:text-base text-gray-800 mb-4">Personal Details</h3>
+              <h3 className="text-sm lg:text-base text-gray-800 mb-4">
+                Personal Details
+              </h3>
               <div className="grid md:grid-cols-2 gap-4">
-                <input type="text" placeholder="First Name" className="input-field text-black" />
-                <input type="text" placeholder="Last Name" className="input-field text-black" />
-                <input type="email" placeholder="Email" className="input-field text-black" />
-                <input type="number" placeholder="Phone No." className="input-field text-black" />
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  className="input-field text-black"
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  className="input-field text-black"
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="input-field text-black"
+                />
+                <input
+                  type="number"
+                  placeholder="Phone No."
+                  className="input-field text-black"
+                />
               </div>
             </div>
             <div className="mt-8">
-              <h3 className="text-sm lg:text-base text-gray-800 mb-4">Shipping Address</h3>
+              <h3 className="text-sm lg:text-base text-gray-800 mb-4">
+                Shipping Address
+              </h3>
               <div className="grid md:grid-cols-2 gap-4">
-                <input type="text" placeholder="Address Line" className="input-field text-black" />
-                <input type="text" placeholder="City" className="input-field text-black" />
-                <input type="text" placeholder="State" className="input-field text-black" />
-                <input type="text" placeholder="Zip Code" className="input-field text-black" />
+                <input
+                  type="text"
+                  placeholder="Address Line"
+                  className="input-field text-black"
+                />
+                <input
+                  type="text"
+                  placeholder="City"
+                  className="input-field text-black"
+                />
+                <input
+                  type="text"
+                  placeholder="State"
+                  className="input-field text-black"
+                />
+                <input
+                  type="text"
+                  placeholder="Zip Code"
+                  className="input-field text-black"
+                />
               </div>
               <div className="flex gap-4 max-md:flex-col mt-8">
-                <button type="button" className="btn-cancel">Cancel</button>
-                <button type="submit" className="btn-submit">Complete Purchase</button>
+                <button type="button" className="btn-cancel">
+                  Cancel
+                </button>
+                <button type="submit" className="btn-submit">
+                  Complete Purchase
+                </button>
               </div>
             </div>
           </form>
